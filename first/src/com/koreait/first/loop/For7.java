67c2d330cef9490926a9fca54eed6ed5d55dd669
Administrator <Administrator@DESKTOP-3EAFP4R>
2020-11-27 14:23:47 +0900

    For7

--- first/src/com/koreait/first/loop/For7.java
+++ first/src/com/koreait/first/loop/For7.java
@@ -3,8 +3,30 @@ package com.koreait.first.loop;
 public class For7 {
 
 	public static void main(String[] args) {
-		int star = 6;
-
+		int star = 10;
+		for(int i=1; i<=star; i++) {
+			for(int z=0; z<star; z++) {
+				if(star - i > z) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("----------");
+		
+		for(int i=star; i>0; i--) {
+			for(int z=1; z<=star; z++) {
+				if(i > z) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
